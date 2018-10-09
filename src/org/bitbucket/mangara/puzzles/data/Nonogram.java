c0c5342dbb9a8bf05d2a3334b38f17e79b60d02e
Sander Verdonschot <sander.verdonschot@gmail.com>
2018-10-08 22:14:42 -0400

    Extract Nonogram reading to dedicated class.

--- src/org/bitbucket/mangara/puzzles/data/Nonogram.java
+++ src/org/bitbucket/mangara/puzzles/data/Nonogram.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Sander Verdonschot.
+ * Copyright 2018 Sander Verdonschot <sander.verdonschot at gmail.com>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
